0d2008dadfa2edc6079bb7c23b5e8d1ac35cfffe
Artem Koval <dakovlen@gmail.com>
2023-03-22 17:46:22 +0200

    Main: initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const str = 'Str'
+// console.log(str + ' Test 4')
+//let userName: string = 'Name'
+var isBirhday = true;
+var age = 40;
+var userName = 'John';
+if (isBirhday) {
+    console.log("Congrats ".concat(userName.toLowerCase(), ", age: ").concat(age + 1));
+}
---
+++ index.ts
@@ -0,0 +1,13 @@
+// const str = 'Str'
+
+// console.log(str + ' Test 4')
+
+//let userName: string = 'Name'
+
+const isBirhday: boolean = true
+const age: number = 40
+const userName: string = 'John'
+
+if (isBirhday) {
+  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}` );
+}
